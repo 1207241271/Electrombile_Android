fc22f44ed7a6aa19cb841ff1f81ecf59da5d7877
Jason <Jerson.G@gmail.com>
2016-08-24 15:44:39 +0800

    remove unused code

--- app/src/main/java/com/xunce/electrombile/activity/SplashActivity.java
+++ app/src/main/java/com/xunce/electrombile/activity/SplashActivity.java
@@ -1,19 +1,12 @@
 package com.xunce.electrombile.activity;
 
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 
 import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.LogUtil;
@@ -22,46 +15,16 @@ import com.xunce.electrombile.R;
 import com.xunce.electrombile.activity.account.LoginActivity;
 import com.xunce.electrombile.activity.account.VerifiedActivity;
 import com.xunce.electrombile.manager.SettingManager;
-import com.xunce.electrombile.utils.useful.JSONUtils;
 import com.xunce.electrombile.utils.useful.NetworkUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Random;
 
 import cn.jpush.android.api.InstrumentedActivity;
 
 public class SplashActivity extends InstrumentedActivity {
 
-    private final int UPDATE = 0;
-    private final int UN_UPDATE = 1;
-    private boolean isUpdate;
-    private File file;
-    private ProgressDialog progressDialog;
-    private SettingManager setManager;
-    Handler MyHandler = new Handler() {
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what) {
-                case UPDATE:
-                    Bundle bundle = msg.getData();
-                    isUpdate = bundle.getBoolean("isupdate");
-                    if (isUpdate) {
-                        isUpdate = false;
-                    } else {
-                        MyHandler.sendEmptyMessageDelayed(UN_UPDATE, 2000);
-                    }
-                    break;
-                case UN_UPDATE:
 
-                    break;
-            }
-        }
-    };
+    private SettingManager setManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
